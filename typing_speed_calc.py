55bd036010890813bacc343c2a050b435fede455
Shravan <dev.shravan@protonmail.com>
2023-03-03 07:18:57 +0000

    minor fixes

--- typing_speed_calc.py
+++ typing_speed_calc.py
@@ -1,30 +1,37 @@
-import time, random
+import time
+import random
 
 with open(r"typing_speed_phrases.txt") as p:
     ps = p.read()
     phrase_list = ps.split("\n")
 
-def save_history(name:str, wpm, mistakes:int):
+
+def save_history(name: str, wpm, mistakes: int):
     """
     Saves the result of the typing speed test.
     """
     text = f"{name.capitalize()} had an average typing speed of {wpm} wpm with {mistakes} mistakes commited.\n"
     try:
-        with open(r'typing_speed_history.txt', 'a') as f:
+        with open(r"typing_speed_history.txt", "a") as f:
             f.write(text)
     except Exception as e:
         print(e)
+
 
 def read_history():
     """
     Reads the typing speed test history.
     """
-    with open(r"typing_speed_history.txt", 'r') as f:
-        fc = f.read()
-        print(fc)
-        
+    try:
+        with open(r"typing_speed_history.txt", "r") as f:
+            fc = f.read()
+            print(fc)
+    
+    except FileNotFoundError:
+        print("No history yet.")
 
-def calc_typing_speed(name:str):
+
+def calc_typing_speed(name: str):
     """
     Main code.
     """
@@ -41,12 +48,14 @@ def calc_typing_speed(name:str):
                 continue
             else:
                 print(f"Phrase {i}: {phrase}")
-                phrase_ques = (phrase.split(" "))
+                phrase_ques = phrase.split(" ")
                 already_asked.append(phrase)
                 break
 
         while 1:
-            input("Press enter and start typing the phrase immediately and again press enter once you've written the phrase.")
+            input(
+                "Press enter and start typing the phrase immediately and again press enter once you've written the phrase."
+            )
             # time taken by user for response
             init = time.time()
             user_ans = input()
@@ -56,7 +65,7 @@ def calc_typing_speed(name:str):
 
             if len(user_ans) == len(phrase_ques):
                 difference = finish - init
-                wpm = (len(phrase_ques)*60)//difference
+                wpm = (len(phrase_ques) * 60) // difference
                 sum_wpm += wpm
                 for i in range(len(user_ans)):
                     # checking for mistakes
@@ -64,22 +73,22 @@ def calc_typing_speed(name:str):
                         pass
                     else:
                         mistakes += 1
-                        mistake_dict.update({phrase_ques[i]:user_ans[i]})
+                        mistake_dict.update({phrase_ques[i]: user_ans[i]})
                 break
 
             else:
                 print("You didn't type the whole phrase!")
 
         print("\n---------------------------------------------\n")
-                
-    average_wpm = sum_wpm/5
+
+    average_wpm = sum_wpm / 5
     result = f"Hey {name.capitalize()}, your average words per minute typing speed counts {average_wpm}.\n You commited {mistakes} mistakes.\n"
     print(result)
 
     if len(mistake_dict.keys()) != 0:
         print("Your typing mistakes are:\n")
-        for key in mistake_dict.keys():
-            print(f"{key} --> {mistake_dict[key]}")
+        for key, value in mistake_dict.items():
+            print(f"{key} --> {value}")
 
     save_history(name, average_wpm, mistakes)
 
@@ -89,7 +98,11 @@ if __name__ == "__main__":
     name = input("Enter your name: ")
 
     while True:
-        task = int(input("What to do?\n 1. Take a typing speed test \n 2. Read old typing speed test history \n 3. Quit \n"))
+        task = int(
+            input(
+                "What to do?\n 1. Take a typing speed test \n 2. Read old typing speed test history \n 3. Quit \n"
+            )
+        )
 
         if task == 1:
             calc_typing_speed(name)
@@ -99,3 +112,4 @@ if __name__ == "__main__":
 
         else:
             print("Thanks for visiting!")
+            quit()
